addbccfd7a11994d0e0b05c93cea77a8fc80c874
Pickacho <40912416+Pickacho@users.noreply.github.com>
2022-04-22 13:00:15 +0300

    0.6

--- first_install.sh
+++ first_install.sh
@@ -35,15 +35,39 @@ mkdir -p  /home/ec2-user/jenkins/
 chown -R  5050:5050 /home/ec2-user/postgresql/pgadmin
 
 docker run  --rm  --name postgresql -e POSTGRES_USER=ran -e POSTGRES_PASSWORD=marpaihealth -p 5432:5432 -v /home/ec2-user/postgresql/data:/var/lib/postgresql/data -d postgres
+
+
+
 docker run --rm --name my-pgadmin -p 80:80   --memory="0.3g" --cpus="0.3" -v /home/ec2-user/postgresql/pgadmin/var/lib/pgadmin/:/var/lib/pgadmin  -v "/home/ec2-user/postgresql/pgadmin/home/:$HOME"  -e 'PGADMIN_DEFAULT_EMAIL=admin@admin.admin' -e 'PGADMIN_DEFAULT_PASSWORD=postgresmaster' -d dpage/pgadmin4
 
 
 #get postgresql container ip for PGadmin connection
 containerIP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql`
-echo use $containerIP as postgres instance ip
+echo use $containerIP as postgres instance ip >> /tmp/postgres_internalip.log
 
 
-# docker network create jenkins
+wget https://raw.githubusercontent.com/Pickacho/marpaihealth/master/Dockerfile -O /home/ec2-user/jenkins/Dockerfile
+cd /home/ec2-user/jenkins/
+docker build -t myjenkins-blueocean:2.332.2-1 .
+
+docker network create jenkins
+docker run \
+  --name jenkins-blueocean \
+  --rm \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --memory="0.6g" --cpus="0.6" \
+  myjenkins-blueocean:2.332.2-1 
+
+sleep 20 
+ docker exec -it jenkins-blueocean  cat /var/jenkins_home/secrets/initialAdminPassword  ; echo ""
 
 # docker run \
 #   --name jenkins-docker \
@@ -60,9 +84,7 @@ echo use $containerIP as postgres instance ip
 #   --storage-driver overlay2
 
 
-wget https://raw.githubusercontent.com/Pickacho/marpaihealth/master/Dockerfile -O /home/ec2-user/jenkins/Dockerfile
-cd /home/ec2-user/jenkins/
-docker build -t myjenkins-blueocean:2.332.2-1 .
+
 
 # cat << EOF > /home/ec2-user/jenkins/Dockerfile
 # FROM jenkins/jenkins:2.332.2-jdk11
@@ -79,21 +101,5 @@ docker build -t myjenkins-blueocean:2.332.2-1 .
 # RUN jenkins-plugin-cli --plugins "blueocean:1.25.3 docker-workflow:1.28"
 # EOF
 
-docker run \
-  --name jenkins-blueocean \
-  --rm \
-  --detach \
-  --network jenkins \
-  --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client \
-  --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 \
-  --publish 50000:50000 \
-  --volume jenkins-data:/var/jenkins_home \
-  --volume jenkins-docker-certs:/certs/client:ro \
-  myjenkins-blueocean:2.332.2-1 
-
-sleep 20 
- docker exec -it jenkins-blueocean  cat /var/jenkins_home/secrets/initialAdminPassword  ; echo ""
 
 #  1807b2090c00428581aff010c5c3fb2d
